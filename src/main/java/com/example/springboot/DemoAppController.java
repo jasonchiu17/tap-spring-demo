954f1fc74a1dd667dd9bac72412dbcdcb5494c9c
Jason Chiu <jason.chiu172@gmail.com>
2022-12-22 17:20:43 +0800

    JB-26 add zerone template

---
+++ src/main/java/com/example/springboot/DemoAppController.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoAppController {
+   @RequestMapping(value = {"/index", "/"} )
+   public String index() {
+      /return "index";
+      // return "index-v2";
+   }
+}
